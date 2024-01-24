fdc98b355749aa2a8682b13346be35abd6a3234f
aarrieche <arriechealexandre@gmail.com>
2024-01-23 21:20:11 -0300

    :sparkles: feat: Exception handling - update

--- src/main/java/com/aarriechespring/spring/services/UserService.java
+++ src/main/java/com/aarriechespring/spring/services/UserService.java
@@ -13,6 +13,8 @@ import com.aarriechespring.spring.repositories.UserRepository;
 import com.aarriechespring.spring.services.exceptions.DatabaseException;
 import com.aarriechespring.spring.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity,obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 		
 	}
 
